f541166321b6c900dd1d2c2315da6401e5043dd4
Tsungyu Yu <ytyubox@gmail.com>
2020-12-11 18:54:01 +0800

    lower xcode version 10.15

---
+++ AdventOfCode2020Tests/Day02/Day02Tests.swift
@@ -0,0 +1,17 @@
+//
+// Created by 游宗諭 in 2020/12/11
+//
+//
+// Using Swift 5.0
+//
+// Running on macOS 10.15
+
+
+import XCTest
+
+class Day02Tests: XCTestCase {
+    func test() throws {
+        
+    }
+
+}
